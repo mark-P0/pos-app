c0445a08c8c8aa59f6ed587462d4a0b8cf94ffbc
Mark P <mj.poso18@gmail.com>
2021-06-27 12:31:45 +0800

    Reset page to clean slate

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,11 @@
 import React from 'react';
 import './App.css';
-import { contentProportion } from './components/common';
-import Sidebar from './components/Sidebar';
 
-// Change this here or on the `public` folder?
-// How about the favicon?
-document.title = 'POSApp';
-
+/* TODO: Typing? */
 export default function App() {
   return (
-    <div
-      style={{
-        // padding: 0,
-        // margin: 0,
-        // listStyle: 'none',
-        display: 'flex', // Important? Signifies use of flexbox?
-
-        // flex: 1,
-        // flexDirection: 'column',
-        height: '100vh',
-      }}
-    >
-      <title>POSApp</title>
-      <Sidebar />
-      <div
-        style={{
-          flex: `${100 - contentProportion.sidebar}%`,
-          // backgroundColor: 'lightyellow',
-        }}
-      >
-        {/* <p>{sectionSizes.sidebar}</p> */}
-      </div>
+    <div>
+      <div>'Hello, world!'</div>
     </div>
-  );
+  )
 }
