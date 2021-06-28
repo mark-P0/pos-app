e6199622a90dc9e056f584a96aaf2c3c68ec8335
Mark P <mj.poso18@gmail.com>
2021-06-29 01:45:06 +0800

    Initialized branch work

---
+++ src/components/Content.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+// export default function Content() {
+//   return (
+//     <div></div>
+//   )
+// }
+
+type ContentPropTypes = {
+  style?: {};
+};
+
+const Content: React.FC<ContentPropTypes> = () => {
+  return <div></div>;
+};
+
+export default Content;
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Navbar.css';
+
+// export default function Navbar() {
+//   return (
+//     <div></div>
+//   )
+// }
+
+type NavbarPropTypes = {
+  style?: {};
+};
+
+const Navbar: React.FC<NavbarPropTypes> = () => {
+  return <div></div>;
+};
+
+export default Navbar;
