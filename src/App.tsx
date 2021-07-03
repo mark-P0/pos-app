0c324c86fce0a76ba62c48e6d8f4794558cabe2e
Mark P <mj.poso18@gmail.com>
2021-07-03 23:21:00 +0800

    Moved navbar to separate component

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,12 @@
-import {
-  Box,
-  Center,
-  ChakraProvider,
-  Flex,
-  IconButton,
-  Text,
-} from '@chakra-ui/react';
-import { FaCashRegister, FaHome, FaReceipt, FaWarehouse } from 'react-icons/fa';
+import { Box, Center, ChakraProvider, Flex, Text } from '@chakra-ui/react';
+import Navbar from './components/Navbar';
 import customizedTheme from './components/theme';
 import './utilities/standard.css';
-
-/* Central buttons */
-let NavbarButtonIconMap = {
-  home: <FaHome />,
-  pos: <FaCashRegister />,
-  transactions: <FaReceipt />,
-  inventory: <FaWarehouse />,
-};
-
-type NavbarButtonPropType = {
-  // https://dev.to/multimo/how-to-build-typescript-string-literal-types-from-objects-values-361l
-  name: keyof typeof NavbarButtonIconMap;
-  a11y: string;
-};
-
-const NavbarButton: React.FC<NavbarButtonPropType> = (props) => {
-  let { name, a11y } = props;
-
-  return (
-    <IconButton
-      icon={NavbarButtonIconMap[name]}
-      aria-label={a11y}
-      isRound={true}
-      colorScheme="blackAlpha"
-      m="0 0.75rem"
-    />
-  );
-};
 
 /* App proper */
 const App: React.FC = () => {
   // TODO: Move to theme? Chakra provider?
+  // TODO: Redundant! Remove this
   const sizes = {
     navbarHeight: '80px',
     navbarCentralWidth: '50%',
@@ -62,56 +28,7 @@ const App: React.FC = () => {
         bgColor="midnightblue"
       >
         {/* Navigation bar */}
-        <Flex
-          h={sizes.navbarHeight}
-          color="white"
-          // bgColor={randomizedCSSrgb()}
-          flexDirection="row"
-        >
-          {/* Logo */}
-          <Flex
-            w={`calc((100% - ${sizes.navbarCentralWidth}) / 2)`}
-            // bgColor={randomizedCSSrgb()}
-          >
-            <Center w="100%">
-              <Text>Logo</Text>
-            </Center>
-          </Flex>
-
-          {/* Button section */}
-          <Box
-            w={sizes.navbarCentralWidth}
-            // bgColor={randomizedCSSrgb()}
-          >
-            {/* Button container */}
-            <Center
-              maxW="fit-content"
-              // h={sizes.navbarButtonContainer.height}
-              h="100%"
-              m="0 auto"
-              p={`0 ${sizes.navbarButtonContainer.sidePadding}`}
-              borderRadius={`0 0 ${sizes.navbarButtonContainer.radius} ${sizes.navbarButtonContainer.radius}`}
-              bgColor="deepskyblue"
-            >
-              <Flex flexDirection="row-reverse">
-                <NavbarButton name="home" a11y="Go back to the home page" />
-                <NavbarButton name="pos" a11y="Render sales services" />
-                <NavbarButton name="transactions" a11y="Show previous sales" />
-                <NavbarButton name="inventory" a11y="View current inventory" />
-              </Flex>
-            </Center>
-          </Box>
-
-          {/* User */}
-          <Flex
-            w={`calc((100% - ${sizes.navbarCentralWidth}) / 2)`}
-            // bgColor={randomizedCSSrgb()}
-          >
-            <Center w="100%">
-              <Text>User details</Text>
-            </Center>
-          </Flex>
-        </Flex>
+        <Navbar />
 
         {/* Content section */}
         <Box h={`calc(100% - ${sizes.navbarHeight})`} p="1%">
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,46 +1,119 @@
-import React from 'react';
-import styles from './Navbar.module.scss';
+import {
+  Box,
+  ButtonGroup,
+  Center,
+  Flex,
+  IconButton,
+  Text,
+} from '@chakra-ui/react';
+import { FaCashRegister, FaHome, FaReceipt, FaWarehouse } from 'react-icons/fa';
+import { randomizedCSSrgb } from '../utilities/utils';
 
-const NavbarLogo: React.FC = () => {
-  return <div className={styles.navbarLogo}>Logo</div>;
+let NavbarButtonIconMap = {
+  home: <FaHome />,
+  pos: <FaCashRegister />,
+  transactions: <FaReceipt />,
+  inventory: <FaWarehouse />,
 };
 
-const NavbarButtons: React.FC = () => {
+type NavbarButtonPropType = {
+  // https://dev.to/multimo/how-to-build-typescript-string-literal-types-from-objects-values-361l
+  name: keyof typeof NavbarButtonIconMap;
+  a11y: string;
+};
+
+const NavbarButton: React.FC<NavbarButtonPropType> = (props) => {
+  let { name, a11y } = props;
+
   return (
-    <div className={styles.navbarButtons}>
-      <div className={styles.navbarButtonContainer}>
-        <div className={styles.navbarButton}>
-          <button>Dashboard</button>
-        </div>
-        <div className={styles.navbarButton}>
-          <button>POS</button>
-        </div>
-        <div className={styles.navbarButton}>
-          <button>Transactions</button>
-        </div>
-        <div className={styles.navbarButton}>
-          <button>Inventory</button>
-        </div>
-      </div>
-    </div>
+    <IconButton
+      icon={NavbarButtonIconMap[name]}
+      aria-label={a11y}
+      isRound={true}
+      colorScheme="blackAlpha"
+      m="0 0.75rem"
+      // variant="ghost"
+      // color="white"
+    />
   );
 };
 
-const NavbarUser: React.FC = () => {
-  return <div className={styles.navbarUser}>User</div>;
-};
+const Navbar: React.FC = () => {
+  // TODO: Move to theme? Chakra provider?
+  // TODO: Redundant! Remove this
+  const sizes = {
+    navbarHeight: '80px',
+    navbarCentralWidth: '50%',
+    navbarButtonContainer: {
+      height: '90%',
+      sidePadding: '4%',
+      radius: '1rem',
+    },
+    navbarButtonSideMargin: '0.75rem',
+    contentRadius: '2rem',
+  };
 
-type NavbarProps = {
-  additionalClass?: string;
-};
-
-const Navbar: React.FC<NavbarProps> = ({ additionalClass }) => {
   return (
-    <div className={[additionalClass, styles.navbar].join(' ')}>
-      <NavbarLogo />
-      <NavbarButtons />
-      <NavbarUser />
-    </div>
+    <Flex
+      h={sizes.navbarHeight}
+      color="white"
+      // bgColor={randomizedCSSrgb()}
+      flexDirection="row"
+    >
+      {/* Logo */}
+      <Flex
+        w={`calc((100% - ${sizes.navbarCentralWidth}) / 2)`}
+        // bgColor={randomizedCSSrgb()}
+      >
+        <Center w="100%">
+          <Text>Logo</Text>
+        </Center>
+      </Flex>
+
+      {/* Button section */}
+      <Box
+        w={sizes.navbarCentralWidth}
+        // bgColor={randomizedCSSrgb()}
+      >
+        {/* Button container */}
+        <Center
+          maxW="fit-content"
+          // h={sizes.navbarButtonContainer.height}
+          h="100%"
+          m="0 auto"
+          p={`0 ${sizes.navbarButtonContainer.sidePadding}`}
+          borderRadius={`0 0 ${sizes.navbarButtonContainer.radius} ${sizes.navbarButtonContainer.radius}`}
+          bgColor="lightsteelblue"
+        >
+          <Flex flexDirection="row-reverse">
+            <NavbarButton name="home" a11y="Go back to the home page" />
+            <NavbarButton name="pos" a11y="Render sales services" />
+            <NavbarButton name="transactions" a11y="Show previous sales" />
+            <NavbarButton name="inventory" a11y="View current inventory" />
+          </Flex>
+        </Center>
+
+        {/* <ButtonGroup
+          //
+          bgColor={randomizedCSSrgb()}
+        >
+          <NavbarButton name="home" a11y="Go back to the home page" />
+          <NavbarButton name="pos" a11y="Render sales services" />
+          <NavbarButton name="transactions" a11y="Show previous sales" />
+          <NavbarButton name="inventory" a11y="View current inventory" />
+        </ButtonGroup> */}
+      </Box>
+
+      {/* User */}
+      <Flex
+        w={`calc((100% - ${sizes.navbarCentralWidth}) / 2)`}
+        // bgColor={randomizedCSSrgb()}
+      >
+        <Center w="100%">
+          <Text>User details</Text>
+        </Center>
+      </Flex>
+    </Flex>
   );
 };
 
