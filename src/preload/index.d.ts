2bb533d1ebfec078e0ff592ef34f838c964226a9
Mark P <mj.poso18@gmail.com>
2023-11-02 00:38:50 +0800

    Add wrapper for IPC invocations

--- src/preload/index.d.ts
+++ src/preload/index.d.ts
@@ -1,5 +1,12 @@
+import { Channel, HandlerArgs, HandlerValue } from "../main/ipc-handlers.js";
+
 declare global {
   interface Window {
-    api: unknown;
+    api: {
+      ipcInvoke: <T extends Channel>(
+        channel: T,
+        ...args: HandlerArgs<T>
+      ) => Promise<HandlerValue<T>>;
+    };
   }
 }
--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,7 +1,22 @@
-import { contextBridge } from "electron";
+import { contextBridge, ipcRenderer } from "electron";
+import { Channel, HandlerArgs, HandlerValue } from "../main/ipc-handlers.js";
 
-/** Custom APIs for renderer */
-const api = {};
+async function ipcInvoke<T extends Channel>(
+  channel: T,
+  ...args: HandlerArgs<T>
+): Promise<HandlerValue<T>> {
+  return await ipcRenderer.invoke(channel, ...args);
+}
+
+/**
+ * Custom APIs for renderer
+ *
+ * On new additions to the API:
+ * - Get the inferred type, e.g. via hover definitions (VSCode)
+ * - Copy the inferred type into `src/preload/index.d.ts`
+ *    - This is for IntelliSense in renderer files
+ */
+const api = { ipcInvoke };
 
 /**
  * Use `contextBridge` APIs to expose Electron APIs to
