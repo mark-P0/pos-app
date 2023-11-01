30754c9219cf1a6f1e7d664c78e01b92c6ce06f7
Mark P <mj.poso18@gmail.com>
2023-11-02 00:10:35 +0800

    Remove potentially insecure direct exposure of Electron APIs to renderer

--- src/preload/index.ts
+++ src/preload/index.ts
@@ -1,4 +1,3 @@
-import { electronAPI } from "@electron-toolkit/preload";
 import { contextBridge } from "electron";
 
 /** Custom APIs for renderer */
@@ -11,14 +10,11 @@ const api = {};
  */
 if (process.contextIsolated) {
   try {
-    contextBridge.exposeInMainWorld("electron", electronAPI);
     contextBridge.exposeInMainWorld("api", api);
   } catch (error) {
     console.error(error);
   }
 } else {
-  // @ts-ignore (define in dts)
-  window.electron = electronAPI;
   // @ts-ignore (define in dts)
   window.api = api;
 }
