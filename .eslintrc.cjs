d678c790bceb35f5e4a200415e2df4dfacc240f6
Mark P <mj.poso18@gmail.com>
2023-11-01 18:07:57 +0800

    Delete opinionated ESLint settings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,7 @@ module.exports = {
     "eslint:recommended",
     "plugin:react/recommended",
     "plugin:react/jsx-runtime",
-    "@electron-toolkit/eslint-config-ts/recommended",
-    "@electron-toolkit/eslint-config-prettier",
+    // "@electron-toolkit/eslint-config-ts/recommended",
+    // "@electron-toolkit/eslint-config-prettier",
   ],
 };
