b57cbac62dfc39a4b1e1b47a391ebbbda3b5b5ee
Mark P <mj.poso18@gmail.com>
2023-11-02 04:31:30 +0800

    Link fonts to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx,html}"],
   theme: {
     extend: {},
+    fontFamily: {
+      head: ["Barlow Condensed"],
+      body: ["Montserrat"],
+    },
   },
   plugins: [],
 };
