adfb425bf50c6a98370e4cf348e5b3b33cebe689
Mark P <mj.poso18@gmail.com>
2023-11-02 17:09:54 +0800

    Move Tailwind directives to assets folder

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App.js";
 import "./assets/fonts.css";
-import "./tailwind.css";
+import "./assets/tailwind.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
