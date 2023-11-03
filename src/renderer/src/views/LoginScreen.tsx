065dfc225fa7f5e47e074401cce0bfb27d73a1f3
Mark P <mj.poso18@gmail.com>
2023-11-03 22:42:58 +0800

    Further separate login screen components

--- src/renderer/src/views/LoginScreen.tsx
+++ src/renderer/src/views/LoginScreen.tsx
@@ -14,11 +14,6 @@ function LoginForm() {
     changeScreen("feature-select");
   }
 
-  const cls = C(
-    "w-96 grid gap-12 p-12 pb-6",
-    "border-2 border-cyan-950 dark:border-transparent dark:bg-white/10 rounded-xl",
-    "transition",
-  );
   const inputCls = C(
     "px-2 py-1",
     "border-2 border-cyan-950 dark:border-transparent dark:bg-cyan-950",
@@ -29,30 +24,41 @@ function LoginForm() {
     "transition bg-rose-700 hover:bg-rose-600 active:scale-95 text-white",
   );
   return (
+    <form className="grid gap-6 select-none" onSubmit={login}>
+      <section className="grid gap-3">
+        <label className="grid grid-cols-[35%_65%] items-center">
+          <span className="text-sm tracking-widest">Username</span>
+          <input className={inputCls} type="text" name="username" />
+        </label>
+        <label className="grid grid-cols-[35%_65%] items-center">
+          <span className="text-sm tracking-widest">Password</span>
+          <input className={inputCls} type="password" name="password" />
+        </label>
+      </section>
+
+      <div className="flex justify-between items-end">
+        <span className="uppercase text-xs opacity-50 dark:opacity-25">
+          Version 0.0.0
+        </span>
+        <button className={buttonCls}>Log In</button>
+      </div>
+    </form>
+  );
+}
+
+function LoginCard() {
+  const cls = C(
+    "w-96 grid gap-12 p-12 pb-6",
+    "border-2 border-cyan-950 dark:border-transparent dark:bg-white/10 rounded-xl",
+    "transition",
+  );
+  return (
     <section className={cls}>
       <header className="grid place-content-center">
         <code className="text-3xl font-bold tracking-widest">pos-app</code>
       </header>
 
-      <form className="grid gap-6 select-none" onSubmit={login}>
-        <section className="grid gap-3">
-          <label className="grid grid-cols-[35%_65%] items-center">
-            <span className="text-sm tracking-widest">Username</span>
-            <input className={inputCls} type="text" name="username" />
-          </label>
-          <label className="grid grid-cols-[35%_65%] items-center">
-            <span className="text-sm tracking-widest">Password</span>
-            <input className={inputCls} type="password" name="password" />
-          </label>
-        </section>
-
-        <div className="flex justify-between items-end">
-          <span className="uppercase text-xs opacity-50 dark:opacity-25">
-            Version 0.0.0
-          </span>
-          <button className={buttonCls}>Log In</button>
-        </div>
-      </form>
+      <LoginForm />
     </section>
   );
 }
@@ -60,7 +66,7 @@ function LoginForm() {
 export function LoginScreen() {
   return (
     <Screen>
-      <LoginForm />
+      <LoginCard />
     </Screen>
   );
 }
