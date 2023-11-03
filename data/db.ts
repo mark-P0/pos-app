5005d4f75e9c76db9f03c072978a158027f13e22
Mark P <mj.poso18@gmail.com>
2023-11-03 22:36:39 +0800

    Check user password against database hash

--- data/db.ts
+++ data/db.ts
@@ -2,7 +2,7 @@ import Database from "better-sqlite3";
 import { eq } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import path from "path";
-import { hash } from "./auth.js";
+import { hash, isHashOf } from "./auth.js";
 import { NewUser, User, products, users } from "./schema.js";
 
 const { PORTABLE_EXECUTABLE_DIR } = process.env;
@@ -25,4 +25,28 @@ export async function addUser(user: NewUser) {
 
 export async function deleteUser(username: User["username"]) {
   await db.delete(users).where(eq(users.username, username));
+}
+
+export async function getUser(username: User["username"]) {
+  const res = await db.select().from(users).where(eq(users.username, username));
+  if (res.length === 0) {
+    throw new Error(`User \`${username}\` does not exist!`);
+  }
+  /* Should be impossible as the username is a primary key */
+  if (res.length > 1) {
+    throw new Error(`There are multiple users of the name \`${username}\`!`);
+  }
+
+  return res[0];
+}
+
+export async function isUserValid(user: User) {
+  let record: User;
+  try {
+    record = await getUser(user.username);
+  } catch {
+    return false;
+  }
+
+  return await isHashOf(record.password, user.password);
 }
