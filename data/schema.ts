4ad9029b319615157ec69fdf545b93098f6cb000
Mark P <mj.poso18@gmail.com>
2023-11-03 22:31:17 +0800

    Merge `feat/drizzle-kit-introspect`

--- data/schema.ts
+++ data/schema.ts
@@ -1,12 +1,19 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const products = sqliteTable("products", {
-  sku: text("sku").primaryKey(),
-  category: text("category"),
-  name: text("name"),
-  description: text("description"),
-  price: integer("price"),
-  stock: integer("stock"),
+  sku: text("sku").primaryKey().notNull(),
+  category: text("category").notNull(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  price: integer("price").notNull(),
+  stock: integer("stock").notNull(),
   url: text("url"),
 });
 export type Product = typeof products.$inferSelect;
+
+export const users = sqliteTable("users", {
+  username: text("username").primaryKey().notNull(),
+  password: text("password").notNull(),
+});
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
