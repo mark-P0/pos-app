d4344ea9dc5fedca9f15329e4f5a22e762f127f0
Mark P <mj.poso18@gmail.com>
2023-11-03 20:24:16 +0800

    Introspect products table from db

--- data/schema.ts
+++ data/schema.ts
@@ -1,12 +1,12 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const products = sqliteTable("products", {
-  sku: text("sku").primaryKey(),
-  category: text("category"),
-  name: text("name"),
-  description: text("description"),
-  price: integer("price"),
-  stock: integer("stock"),
+  sku: text("sku").primaryKey().notNull(),
+  category: text("category").notNull(),
+  name: text("name").notNull(),
+  description: text("description").notNull(),
+  price: integer("price").notNull(),
+  stock: integer("stock").notNull(),
   url: text("url"),
 });
 export type Product = typeof products.$inferSelect;
