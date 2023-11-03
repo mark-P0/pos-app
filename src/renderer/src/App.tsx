bc44baa0ef7647ec20e217622f1eff96f37d92a4
Mark P <mj.poso18@gmail.com>
2023-11-03 15:01:59 +0800

    Initialize inventory management screen

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,9 +1,11 @@
 import { Screen } from "./components/Screen.js";
 import { FunctionScreen } from "./views/FunctionScreen.js";
+import { IMScreen } from "./views/IMScreen.js";
 import { LoginScreen } from "./views/LoginScreen.js";
 import { POSScreen } from "./views/POSScreen.js";
 
 export function App() {
+  return <IMScreen />;
   return <POSScreen />;
   return <FunctionScreen />;
   return <LoginScreen />;
