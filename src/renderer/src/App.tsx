b08c4ca29d017196791790c9bb294e3a1b69967d
Mark P <mj.poso18@gmail.com>
2023-11-03 15:01:09 +0800

    Initialize point-of-sales screen

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,8 +1,10 @@
 import { Screen } from "./components/Screen.js";
 import { FunctionScreen } from "./views/FunctionScreen.js";
 import { LoginScreen } from "./views/LoginScreen.js";
+import { POSScreen } from "./views/POSScreen.js";
 
 export function App() {
+  return <POSScreen />;
   return <FunctionScreen />;
   return <LoginScreen />;
   return <Screen />;
