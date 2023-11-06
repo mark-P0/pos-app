b0148a73b4ecadca5eeeb642b253c37c6fff16e6
Mark P <mj.poso18@gmail.com>
2023-11-06 17:18:36 +0800

    Remove state from dark mode toggle

--- src/renderer/src/components/DarkModeToggle.tsx
+++ src/renderer/src/components/DarkModeToggle.tsx
@@ -1,29 +1,15 @@
 import { C } from "@renderer/utils.js";
-import { useEffect, useState } from "react";
 import { LuMoon, LuSun } from "react-icons/lu";
 
 const { ipcInvoke } = window.api;
 
 export function DarkModeToggle() {
-  const [isDarkMode, setIsDarkMode] = useState<boolean | null>(null);
-  /** Mirror Electron internal dark mode state as a React state */
-  async function reflectDarkModeState() {
-    const isDarkMode = await ipcInvoke("dark-mode:getSetting");
-    setIsDarkMode(isDarkMode);
-  }
-  /** Once, initialize React state to that of the current Electron dark mode state */
-  useEffect(() => {
-    reflectDarkModeState();
-  }, []);
-  async function toggle() {
-    /* Could also just get a return value from the IPC handler and directly set that as state instead */
-    await ipcInvoke("dark-mode:toggle");
-    reflectDarkModeState();
+  function toggle() {
+    ipcInvoke("dark-mode:toggle");
   }
 
   const cls = C(
     "relative",
-    isDarkMode === null && "hidden", // Hide button if state not initialized yet
     "overflow-hidden w-12 aspect-square rounded-full",
     "transition hover:bg-cyan-950/10 dark:hover:bg-white/20 active:scale-90",
   );
