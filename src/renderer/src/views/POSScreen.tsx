94e6a9746578a88cbd52f8567aa8e2bcaba80cdb
Mark P <mj.poso18@gmail.com>
2023-11-06 23:29:46 +0800

    Divide POS screen into 3 columns

--- src/renderer/src/views/POSScreen.tsx
+++ src/renderer/src/views/POSScreen.tsx
@@ -3,7 +3,11 @@ import { Screen } from "@renderer/components/Screen.js";
 export function POSScreen() {
   return (
     <Screen withLogoutButton withFeaturesButton>
-      <code>pos screen</code>
+      <section className="h-full grid grid-cols-5">
+        <div className="col-span-2 bg-red-500/25">receipt</div>
+        <div className="col-span-2 bg-green-500/25">product-list</div>
+        <div className="bg-yellow-500/25">buttons</div>
+      </section>
     </Screen>
   );
 }
