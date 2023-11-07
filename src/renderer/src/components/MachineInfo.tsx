f7f8a2a63051dfd70fdbb4b36d2e265b86a0a1b2
Mark P <mj.poso18@gmail.com>
2023-11-08 02:36:07 +0800

    Add datetime to receipts

--- src/renderer/src/components/MachineInfo.tsx
+++ src/renderer/src/components/MachineInfo.tsx
@@ -1,21 +1,28 @@
 import { useAppContext } from "@renderer/contexts/AppContext.js";
+import { useMemo } from "react";
+
+function getCurrentDateTime() {
+  return new Intl.DateTimeFormat(undefined, {
+    dateStyle: "medium",
+    timeStyle: "medium",
+  }).format(new Date());
+}
 
 export function MachineInfo() {
   const { labels } = useAppContext();
   const [appName, appVersion] = labels;
 
+  const datetime = useMemo(getCurrentDateTime, []); // Run only once, on initial render
+
   return (
     <>
-      <p className="normal-case">
-        Served By:{" "}
+      <p className="normal-case text-center">
+        Served by{" "}
         <span className="text-red-500 font-bold">[LOGGED_IN_USER]</span>
       </p>
-      <p className="normal-case">
-        <span className="text-red-500 font-bold">[DATE]</span>{" "}
-        <span className="text-red-500 font-bold">[TIME]</span>
-      </p>
-      <p className="normal-case">
-        {appName} v{appVersion}
+      <p className="normal-case text-center">
+        {/* eslint-disable-next-line react/jsx-no-comment-textnodes */}
+        {datetime} // {appName} v{appVersion}{" "}
       </p>
     </>
   );
