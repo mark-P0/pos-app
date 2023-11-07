0602f26376d1a3d69bcd5fd746fcb2b9cc2e8702
Mark P <mj.poso18@gmail.com>
2023-11-08 01:20:36 +0800

    Initialize receipt layout

--- src/renderer/src/components/ReceiptColumn.tsx
+++ src/renderer/src/components/ReceiptColumn.tsx
@@ -1,3 +1,53 @@
+import { C } from "@renderer/utils.js";
+
+function Receipt() {
+  const cls = C(
+    "bg-white m-6 mt-0 shadow-xl",
+    "font-mono text-black uppercase text-xs leading-tight",
+    "select-none cursor-not-allowed",
+    "p-6",
+  );
+  return (
+    <section className={cls}>
+      <h2 className="text-center">Official Receipt</h2>
+      <br />
+
+      <p className="text-center">This serves as your sales invoice</p>
+      <br />
+
+      <p className="text-red-500 font-bold">[PRODUCT TABLE]</p>
+      <p className="text-red-500 font-bold">[total / cash / change]</p>
+
+      <br />
+
+      <p className="text-red-500 font-bold">[VAT breakdown]</p>
+      <br />
+
+      <p className="text-red-500 font-bold">[Customer info]</p>
+      <br />
+
+      <p className="text-red-500 font-bold">[machine info]</p>
+      <br />
+
+      <p className="text-center">Thank you for shopping!</p>
+
+      <div className="h-96">{/* temporary spacer to scroll */}</div>
+    </section>
+  );
+}
+
 export function ReceiptColumn() {
-  return <div className="bg-red-500/25">receipt</div>;
+  const cls = C(
+    ...[
+      "overflow-y-auto",
+      "scrollbar-thin",
+      "scrollbar-track-cyan-950/10 scrollbar-thumb-cyan-950",
+      "dark:scrollbar-track-white/10 dark:scrollbar-thumb-white",
+    ],
+  );
+  return (
+    <div className={cls}>
+      <Receipt />
+    </div>
+  );
 }
