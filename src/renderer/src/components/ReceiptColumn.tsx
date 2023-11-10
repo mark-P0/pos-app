7759f3abf0826bccc185cba15dba25513ac47ad0
Mark P <mj.poso18@gmail.com>
2023-11-11 01:04:39 +0800

    Show overlay over receipt column when empty cart

--- src/renderer/src/components/ReceiptColumn.tsx
+++ src/renderer/src/components/ReceiptColumn.tsx
@@ -1,15 +1,37 @@
+import { useCartContext } from "@renderer/contexts/CartContext.js";
 import { C, formatPrice } from "@renderer/utils.js";
 import { CartTable } from "./CartTable.js";
 import { MachineInfo } from "./MachineInfo.js";
 import { MapTable } from "./MapTable.js";
 import { StoreDetails } from "./StoreDetails.js";
 
+function CurtainDialog() {
+  const cls = C(
+    "absolute z-10 w-full h-full",
+    "grid place-content-center",
+    "bg-transparent dark:bg-cyan-950/50",
+    "text-cyan-950 dark:text-white",
+    "transition",
+  );
+  return (
+    <dialog className={cls}>
+      <p className="bg-cyan-950/10 px-3 py-2 font-bold">
+        Add items to begin transaction
+      </p>
+    </dialog>
+  );
+}
+
 function Receipt() {
+  const { isCartEmpty } = useCartContext();
+
   const cls = C(
     "bg-white m-6 mt-0 shadow-xl",
     "font-mono text-black uppercase text-xs leading-tight",
     "select-none cursor-not-allowed",
     "p-6",
+    isCartEmpty && "blur-sm",
+    "transition",
   );
   return (
     <section className={cls}>
@@ -51,8 +73,11 @@ function Receipt() {
 }
 
 export function ReceiptColumn() {
+  const { isCartEmpty } = useCartContext();
+
   const cls = C(
     ...[
+      "relative",
       "overflow-y-auto",
       "scrollbar-thin",
       "scrollbar-track-cyan-950/10 scrollbar-thumb-cyan-950",
@@ -61,6 +86,7 @@ export function ReceiptColumn() {
   );
   return (
     <div className={cls}>
+      {isCartEmpty && <CurtainDialog />}
       <Receipt />
     </div>
   );
