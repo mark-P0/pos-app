14708e1823270b471ec378d154b21e707bb83082
Mark P <mj.poso18@gmail.com>
2023-11-11 01:57:35 +0800

    Add Add-To-Cart function on cart context

--- src/renderer/src/contexts/CartContext.tsx
+++ src/renderer/src/contexts/CartContext.tsx
@@ -1,18 +1,26 @@
-import { PropsWithChildren, createContext } from "react";
+import { PropsWithChildren, createContext, useState } from "react";
 import { Product } from "./ProductsContext.js";
 import { useNullableContext } from "./utils.js";
 
 type Cart = Map<Product["sku"], number>;
 function useCart() {
-  const cart: Cart = new Map();
+  const [cart, setCart] = useState<Cart>(new Map());
   const isCartEmpty = cart.size === 0;
 
-  return { cart, isCartEmpty };
+  function addToCart(sku: Product["sku"], qty: number) {
+    const ct = cart.get(sku) ?? 0;
+    cart.set(sku, ct + qty);
+    const newCart = new Map(cart);
+    setCart(newCart);
+  }
+
+  return { cart, isCartEmpty, addToCart };
 }
 
 type CartValues = {
   cart: Cart;
   isCartEmpty: boolean;
+  addToCart: (sku: Product["sku"], qty: number) => void;
 };
 const CartContext = createContext<CartValues | null>(null);
 export function useCartContext() {
@@ -20,8 +28,8 @@ export function useCartContext() {
 }
 export function CartProvider(props: PropsWithChildren) {
   const { children } = props;
-  const { cart, isCartEmpty } = useCart();
+  const { cart, isCartEmpty, addToCart } = useCart();
 
-  const values = { cart, isCartEmpty };
+  const values = { cart, isCartEmpty, addToCart };
   return <CartContext.Provider value={values}>{children}</CartContext.Provider>;
 }
