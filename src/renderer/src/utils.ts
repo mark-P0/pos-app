ca26adb53a9bebbf78442151d71382df2e35dc98
Mark P <mj.poso18@gmail.com>
2023-11-12 19:33:46 +0800

    Extract primary button classes to common file

--- src/renderer/src/utils.ts
+++ src/renderer/src/utils.ts
@@ -40,6 +40,12 @@ export const classes = {
   ),
   interactiveHoverBg: C("hover:bg-cyan-950/10 dark:hover:bg-white/20"),
   button: {
+    primary: C(
+      "bg-rose-700",
+      "enabled:hover:bg-rose-600 enabled:active:scale-95",
+      "disabled:opacity-50",
+      "text-white",
+    ),
     secondary: C(
       "bg-cyan-800",
       "enabled:hover:bg-cyan-700 enabled:active:scale-95",
