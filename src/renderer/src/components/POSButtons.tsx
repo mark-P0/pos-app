dff756581d961e6f7a4845aeafb2571af510634e
Mark P <mj.poso18@gmail.com>
2023-11-12 20:04:44 +0800

    Change buttons root into `<aside>`

--- src/renderer/src/components/POSButtons.tsx
+++ src/renderer/src/components/POSButtons.tsx
@@ -54,15 +54,14 @@ export function POSButtons() {
 
   const resetCls = C("px-2 py-1", classes.button.secondary, "transition");
   return (
-    <form className="grid auto-rows-min">
+    <aside className="grid auto-rows-min">
       <button
-        type="button"
         className={resetCls}
         disabled={isCartEmpty}
         onClick={showResetPrompt}
       >
         Reset
       </button>
-    </form>
+    </aside>
   );
 }
