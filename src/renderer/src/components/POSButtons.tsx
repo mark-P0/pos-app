7f98d164762ed39f9b379aee5b2907ecedbb481b
Mark P <mj.poso18@gmail.com>
2023-11-12 17:34:47 +0800

    Disable reset button when cart is empty

--- src/renderer/src/components/POSButtons.tsx
+++ src/renderer/src/components/POSButtons.tsx
@@ -1,10 +1,13 @@
+import { useCartContext } from "@renderer/contexts/CartContext.js";
 import { C, classes } from "@renderer/utils.js";
 
 export function POSButtons() {
+  const { isCartEmpty } = useCartContext();
+
   const resetCls = C("px-2 py-1", classes.button.secondary, "transition");
   return (
     <form className="grid auto-rows-min">
-      <button type="button" className={resetCls}>
+      <button type="button" className={resetCls} disabled={isCartEmpty}>
         Reset
       </button>
     </form>
