cbec3130ae1a8dc7b5ebb78d182625fb62955d79
Mark P <mj.poso18@gmail.com>
2023-11-12 19:29:52 +0800

    Show prompt for resetting on button click

--- src/renderer/src/components/POSButtons.tsx
+++ src/renderer/src/components/POSButtons.tsx
@@ -1,11 +1,46 @@
 import { useCartContext } from "@renderer/contexts/CartContext.js";
+import { useModalContext } from "@renderer/contexts/ModalContext.js";
 import { C, classes } from "@renderer/utils.js";
 
-export function POSButtons() {
-  const { isCartEmpty, clearCart } = useCartContext();
+function ResetPrompt() {
+  const { changeContent } = useModalContext();
 
-  function reset() {
-    clearCart();
+  function cancel() {
+    changeContent(null);
+  }
+
+  const cancelCls = C("px-4 py-1", classes.button.secondary);
+  const cls = C(
+    "select-none",
+    "w-[60vw]", // 3/5 of full-width
+    "grid gap-3",
+    "p-6 rounded-lg",
+    ...[classes.bg, classes.text, classes.selection],
+    "transition",
+  );
+  return (
+    <article className={cls}>
+      <header>
+        <h3 className="font-head text-3xl">
+          Are you sure you want to reset the current transaction?
+        </h3>
+      </header>
+
+      <footer className="ml-auto grid grid-flow-col auto-cols-fr gap-3">
+        <button className={cancelCls} onClick={cancel}>
+          No
+        </button>
+      </footer>
+    </article>
+  );
+}
+
+export function POSButtons() {
+  const { isCartEmpty } = useCartContext();
+  const { changeContent } = useModalContext();
+
+  function showResetPrompt() {
+    changeContent(<ResetPrompt />);
   }
 
   const resetCls = C("px-2 py-1", classes.button.secondary, "transition");
@@ -15,7 +50,7 @@ export function POSButtons() {
         type="button"
         className={resetCls}
         disabled={isCartEmpty}
-        onClick={reset}
+        onClick={showResetPrompt}
       >
         Reset
       </button>
