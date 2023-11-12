d133ac3048320a9df7dde0a7d2bae752854bbbc6
Mark P <mj.poso18@gmail.com>
2023-11-12 17:33:53 +0800

    Initialize reset button

--- src/renderer/src/components/POSButtons.tsx
+++ src/renderer/src/components/POSButtons.tsx
@@ -1,3 +1,12 @@
+import { C, classes } from "@renderer/utils.js";
+
 export function POSButtons() {
-  return <div className="bg-yellow-500/25">buttons</div>;
+  const resetCls = C("px-2 py-1", classes.button.secondary, "transition");
+  return (
+    <form className="grid auto-rows-min">
+      <button type="button" className={resetCls}>
+        Reset
+      </button>
+    </form>
+  );
 }
