1696f326af25388e0be4a04ed9af68145eb42488
Mark P <mj.poso18@gmail.com>
2023-11-12 18:03:13 +0800

    Initialize context creator

--- src/renderer/src/contexts/utils.tsx
+++ src/renderer/src/contexts/utils.tsx
@@ -21,3 +21,28 @@ export function useNullableContext<T>(
   }
   return values;
 }
+
+/**
+ * - `useValues()` is a custom hook that "provides" the values of the context/provider
+ * - All of the context states and stuff (or even other custom hooks!) will be inside `useValues()`
+ */
+export function createNewContext<T>(useValues: () => T) {
+  const NewContext = createContext<T | null>(null);
+
+  function useNewContext(): T {
+    const values = useContext(NewContext);
+    if (values === null) {
+      throw new Error("Context possibly not provided");
+    }
+    return values;
+  }
+
+  function NewContextProvider(props: PropsWithChildren) {
+    const { children } = props;
+    const values = useValues();
+
+    return <NewContext.Provider value={values}>{children}</NewContext.Provider>;
+  }
+
+  return [useNewContext, NewContextProvider] as const;
+}
