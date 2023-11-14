1a2c610d2fb496e692364bd51e26100a7d9d27ed
Mark P <mj.poso18@gmail.com>
2023-11-14 19:06:54 +0800

    Nest classes inside own scope

--- src/renderer/src/components/DarkModeToggle.tsx
+++ src/renderer/src/components/DarkModeToggle.tsx
@@ -8,20 +8,24 @@ export function DarkModeToggle() {
     ipcInvoke("dark-mode:toggle");
   }
 
-  const iconCls = C("absolute top-0 left-0 p-3", "h-full w-full", "transition");
-  const darkCls = C(iconCls, "text-cyan-400 dark:opacity-100 opacity-0");
-  const lightCls = C(iconCls, "text-amber-400 opacity-100 dark:opacity-0");
-  const cls = C(
-    "relative",
-    "overflow-hidden w-12 aspect-square rounded-full",
-    classes.interactiveHoverBg,
-    "active:scale-90",
-    "transition",
-  );
+  const cls = (() => {
+    const button = C(
+      "relative",
+      "overflow-hidden w-12 aspect-square rounded-full",
+      classes.interactiveHoverBg,
+      "active:scale-90",
+      "transition",
+    );
+    const icon = C("absolute top-0 left-0 p-3", "h-full w-full", "transition");
+    const dark = C(icon, "text-cyan-400 dark:opacity-100 opacity-0");
+    const light = C(icon, "text-amber-400 opacity-100 dark:opacity-0");
+
+    return { button, icon: { dark, light } };
+  })();
   return (
-    <button className={cls} onClick={toggle}>
-      <LuMoon className={darkCls} />
-      <LuSun className={lightCls} />
+    <button className={cls.button} onClick={toggle}>
+      <LuMoon className={cls.icon.dark} />
+      <LuSun className={cls.icon.light} />
     </button>
   );
 }
