ecd9beefef207024cb72db31cf709498eedfc6be
Mark P <mj.poso18@gmail.com>
2023-11-15 07:46:25 +0800

    Create store details in a block

--- src/renderer/src/components/StoreDetails.tsx
+++ src/renderer/src/components/StoreDetails.tsx
@@ -1,22 +1,30 @@
 import { random$intByLength, random$string } from "@renderer/utils/random.js";
 
 /* TODO Extract the following from database? */
-/** https://ileaveandlive.com/2021/01/18/funny-business-names-pandemic-version/ */
-const store$name = "C. Belen-11 Store"; // cspell:disable-line
-/** https://www.grcdi.nl/gsb/philippines.html */
-const store$address = [
-  "979 Kundiman St., Sampaloc", // cspell:disable-line
-  "Manila, Philippines, 1009",
-];
-const store$tin = [random$intByLength(3), random$intByLength(3), random$intByLength(4)].join(" ") // prettier-ignore
-const store$min = random$intByLength(18);
-const store$contact = [
-  "(+63)",
-  random$intByLength(3),
-  random$intByLength(3),
-  random$intByLength(4),
-].join(" ");
-const store$sn = random$string(8);
+let store$name: string,
+  store$address: string[],
+  store$tin: string,
+  store$min: number,
+  store$contact: string,
+  store$sn: string;
+{
+  /** https://ileaveandlive.com/2021/01/18/funny-business-names-pandemic-version/ */
+  store$name = "C. Belen-11 Store"; // cspell:disable-line
+  /** https://www.grcdi.nl/gsb/philippines.html */
+  store$address = [
+    "979 Kundiman St., Sampaloc", // cspell:disable-line
+    "Manila, Philippines, 1009",
+  ];
+  store$tin = [random$intByLength(3), random$intByLength(3), random$intByLength(4)].join(" ") // prettier-ignore
+  store$min = random$intByLength(18);
+  store$contact = [
+    "(+63)",
+    random$intByLength(3),
+    random$intByLength(3),
+    random$intByLength(4),
+  ].join(" ");
+  store$sn = random$string(8);
+}
 
 export function StoreDetails() {
   return (
