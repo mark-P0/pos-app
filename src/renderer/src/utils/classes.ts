d4dbd937af3a42f186368f53e9dca7dbf8e45b01
Mark P <mj.poso18@gmail.com>
2023-11-15 08:18:01 +0800

    Nest classes inside a block

--- src/renderer/src/utils/classes.ts
+++ src/renderer/src/utils/classes.ts
@@ -6,61 +6,74 @@ export function C(...classes: Array<string | boolean | null | undefined>) {
   return strings.join(" ");
 }
 
-export const cls$absoluteCenter = C(
-  "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
-);
-export const cls$scrollbar = C(
-  "overflow-y-scroll",
-  "scrollbar-thin",
-  "scrollbar-track-cyan-950/10 scrollbar-thumb-cyan-950",
-  "dark:scrollbar-track-white/10 dark:scrollbar-thumb-white",
-);
-export const cls$bg = C("bg-white dark:bg-cyan-950");
-export const cls$text = C("text-cyan-950 dark:text-white");
-export const cls$selection = C(
-  /* Inverse of bg and text classes */
-  "selection:bg-cyan-950 dark:selection:bg-white",
-  "selection:text-white dark:selection:text-cyan-950",
-);
-export const cls$card = C(
-  "border-2 border-cyan-950",
-  "dark:border-transparent dark:bg-white/10",
-  "rounded-lg",
-);
-export const cls$interactiveHoverBg = C(
-  "hover:bg-cyan-950/10 dark:hover:bg-white/20",
-  "focus-visible:bg-cyan-950/10 dark:focus-visible:bg-white/20",
-);
+export let cls$absoluteCenter: string,
+  cls$scrollbar: string,
+  cls$bg: string,
+  cls$text: string,
+  cls$selection: string,
+  cls$card: string,
+  cls$interactiveHoverBg: string,
+  cls$button$primary: string,
+  cls$button$secondary: string,
+  cls$button$icon: string,
+  cls$outline: string;
+{
+  cls$absoluteCenter = C(
+    "absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2",
+  );
+  cls$scrollbar = C(
+    "overflow-y-scroll",
+    "scrollbar-thin",
+    "scrollbar-track-cyan-950/10 scrollbar-thumb-cyan-950",
+    "dark:scrollbar-track-white/10 dark:scrollbar-thumb-white",
+  );
+  cls$bg = C("bg-white dark:bg-cyan-950");
+  cls$text = C("text-cyan-950 dark:text-white");
+  cls$selection = C(
+    /* Inverse of bg and text classes */
+    "selection:bg-cyan-950 dark:selection:bg-white",
+    "selection:text-white dark:selection:text-cyan-950",
+  );
+  cls$card = C(
+    "border-2 border-cyan-950",
+    "dark:border-transparent dark:bg-white/10",
+    "rounded-lg",
+  );
+  cls$interactiveHoverBg = C(
+    "hover:bg-cyan-950/10 dark:hover:bg-white/20",
+    "focus-visible:bg-cyan-950/10 dark:focus-visible:bg-white/20",
+  );
 
-export const cls$button$primary = C(
-  "bg-rose-700",
-  ...[
-    "enabled:hover:bg-rose-600",
-    "enabled:focus-visible:bg-rose-600",
-    "enabled:active:scale-95",
-  ],
-  "disabled:opacity-50",
-  "text-white",
-);
-export const cls$button$secondary = C(
-  "bg-cyan-800",
-  ...[
-    "enabled:hover:bg-cyan-700",
-    "enabled:focus-visible:bg-cyan-700",
-    "enabled:active:scale-95",
-  ],
-  "disabled:opacity-50",
-  "text-white",
-);
-export const cls$button$icon = C(
-  "overflow-hidden w-12 aspect-square rounded-full p-3",
-  cls$interactiveHoverBg,
-  "active:scale-90",
-  "transition",
-);
-export const cls$outline = C(
-  "focus-visible:[&_*]:outline-amber-400",
-  "focus-visible:[&_*]:outline", // Removes white "border" (https://stackoverflow.com/a/61971294)
-  "focus-visible:[&_*]:outline-4",
-  "focus-visible:[&_*]:outline-offset-4",
-);
+  cls$button$primary = C(
+    "bg-rose-700",
+    ...[
+      "enabled:hover:bg-rose-600",
+      "enabled:focus-visible:bg-rose-600",
+      "enabled:active:scale-95",
+    ],
+    "disabled:opacity-50",
+    "text-white",
+  );
+  cls$button$secondary = C(
+    "bg-cyan-800",
+    ...[
+      "enabled:hover:bg-cyan-700",
+      "enabled:focus-visible:bg-cyan-700",
+      "enabled:active:scale-95",
+    ],
+    "disabled:opacity-50",
+    "text-white",
+  );
+  cls$button$icon = C(
+    "overflow-hidden w-12 aspect-square rounded-full p-3",
+    cls$interactiveHoverBg,
+    "active:scale-90",
+    "transition",
+  );
+  cls$outline = C(
+    "focus-visible:[&_*]:outline-amber-400",
+    "focus-visible:[&_*]:outline", // Removes white "border" (https://stackoverflow.com/a/61971294)
+    "focus-visible:[&_*]:outline-4",
+    "focus-visible:[&_*]:outline-offset-4",
+  );
+}
