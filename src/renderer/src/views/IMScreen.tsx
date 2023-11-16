29e8ce5c82bf4c111fd14d3d35b7eea39a177b41
Mark P <mj.poso18@gmail.com>
2023-11-16 23:11:48 +0800

    Initialize inventory screen

--- src/renderer/src/views/IMScreen.tsx
+++ src/renderer/src/views/IMScreen.tsx
@@ -3,7 +3,10 @@ import { Screen } from "@renderer/components/Screen.js";
 export function IMScreen() {
   return (
     <Screen withLogoutButton withFeaturesButton>
-      <code>inv mgmt screen</code>
+      <section className="overflow-hidden h-full grid grid-cols-[3fr_1fr]">
+        <div className="bg-red-500/50">product-list</div>
+        <div className="bg-yellow-500/50">actions</div>
+      </section>
     </Screen>
   );
 }
