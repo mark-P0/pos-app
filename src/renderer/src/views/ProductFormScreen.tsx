17eaf9f0095be71b30fc130c55952b652229e304
Mark P <mj.poso18@gmail.com>
2023-11-18 02:29:14 +0800

    Create dedicated screen for product form

---
+++ src/renderer/src/views/ProductFormScreen.tsx
@@ -0,0 +1,9 @@
+import { Screen } from "@renderer/components/Screen.js";
+
+export function ProductFormScreen() {
+  return (
+    <Screen withLogoutButton withFeaturesButton>
+      <code>product-form-screen</code>
+    </Screen>
+  );
+}
