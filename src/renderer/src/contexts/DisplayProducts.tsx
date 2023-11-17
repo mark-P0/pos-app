e148b743f9f3edab902612bdb49ee62019b0d267
Mark P <mj.poso18@gmail.com>
2023-11-17 18:27:02 +0800

    Sort display products by key

--- src/renderer/src/contexts/DisplayProducts.tsx
+++ src/renderer/src/contexts/DisplayProducts.tsx
@@ -5,7 +5,7 @@ import { createNewContext } from "./utils.js";
 /** Passthrough for all products */
 function useDisplayProducts() {
   const { products: allProducts, categories } = useProductsContext();
-  const products = [...allProducts];
+  let products = [...allProducts];
 
   const sortOrders = ["Ascending", "Descending"] as const;
   type SortOrder = (typeof sortOrders)[number];
@@ -17,6 +17,19 @@ function useDisplayProducts() {
 
   type Category = (typeof categories)[number];
   const [category, setCategory] = useState<Set<Category>>(new Set());
+
+  /* Always sort ascending first */
+  /* TODO Skip sorting if key is not changed? */
+  /* TODO Use switch-case instead? */
+  if (sortKey === "SKU") {
+    products = products.sort((a, b) => (a.sku < b.sku ? -1 : 1));
+  } else if (sortKey === "Name") {
+    products = products.sort((a, b) => (a.name < b.name ? -1 : 1));
+  } else if (sortKey === "Price") {
+    products = products.sort((a, b) => (a.price < b.price ? -1 : 1));
+  } else if (sortKey === "Category") {
+    products = products.sort((a, b) => (a.category < b.category ? -1 : 1));
+  }
 
   return {
     products,
