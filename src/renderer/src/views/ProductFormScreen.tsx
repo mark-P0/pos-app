1d986e4e6fc4596810182ec12722f3c6a1e5ed4f
Mark P <mj.poso18@gmail.com>
2023-11-18 03:51:11 +0800

    Extract form classes to variables

--- src/renderer/src/views/ProductFormScreen.tsx
+++ src/renderer/src/views/ProductFormScreen.tsx
@@ -1,61 +1,56 @@
 import { Screen } from "@renderer/components/Screen.js";
-import { cls$card, cls$interactiveHoverBg } from "@renderer/utils/classes.js";
+import {
+  C,
+  cls$card,
+  cls$interactiveHoverBg,
+} from "@renderer/utils/classes.js";
 
 function ProductForm() {
+  const cls$label = C(
+    "grid grid-cols-[auto_1fr] items-center gap-3",
+    "p-1 pl-3",
+    cls$card,
+    cls$interactiveHoverBg,
+    "transition",
+  );
+  const cls$label$textarea = C(
+    "flex flex-col gap-3",
+    "p-2 px-3",
+    cls$card,
+    cls$interactiveHoverBg,
+    "transition",
+  );
+  const cls$input = C("px-2 py-1", "bg-transparent");
   return (
     <form className="h-full grid grid-cols-3 gap-3 p-6 pt-0">
-      <label
-        className={`grid grid-cols-[auto_1fr] items-center gap-3 p-1 pl-3 ${cls$card} ${cls$interactiveHoverBg} transition col-span-2`}
-      >
+      <label className={`${cls$label} col-span-2`}>
         <span className="font-bold">SKU</span>
-        <input type="text" className="px-2 py-1 bg-transparent" name="sku" />
+        <input type="text" className={cls$input} name="sku" />
       </label>
-      <label
-        className={`grid grid-cols-[auto_1fr] items-center gap-3 p-1 pl-3 ${cls$card} ${cls$interactiveHoverBg} transition col-span-2`}
-      >
+      <label className={`${cls$label} col-span-2`}>
         <span className="font-bold">Name</span>
-        <input type="text" className="px-2 py-1 bg-transparent" name="name" />
+        <input type="text" className={cls$input} name="name" />
       </label>
-      <label
-        className={`grid grid-cols-[auto_1fr] items-center gap-3 p-1 pl-3 ${cls$card} ${cls$interactiveHoverBg} transition col-span-2`}
-      >
+      <label className={`${cls$label} col-span-2`}>
         <span className="font-bold">Category</span>
-        <input
-          type="text"
-          className="px-2 py-1 bg-transparent"
-          name="category"
-        />
+        <input type="text" className={cls$input} name="category" />
       </label>
-      <label
-        className={`grid grid-cols-[auto_1fr] items-center gap-3 p-1 pl-3 ${cls$card} ${cls$interactiveHoverBg} transition`}
-      >
+      <label className={cls$label}>
         <span className="font-bold">Price</span>
-        <input
-          type="number"
-          className="px-2 py-1 bg-transparent"
-          name="price"
-        />
+        <input type="number" className={cls$input} name="price" />
       </label>
-      <label
-        className={`grid grid-cols-[auto_1fr] items-center gap-3 p-1 pl-3 ${cls$card} ${cls$interactiveHoverBg} transition`}
-      >
+      <label className={cls$label}>
         <span className="font-bold">Stock</span>
-        <input
-          type="number"
-          className="px-2 py-1 bg-transparent"
-          name="stock"
-        />
+        <input type="number" className={cls$input} name="stock" />
       </label>
-      <label
-        className={`flex-1 flex flex-col gap-3 p-2 px-3 ${cls$card} ${cls$interactiveHoverBg} transition col-span-3 row-span-6`}
-      >
+      <label className={`${cls$label$textarea} col-span-3 row-span-6`}>
         <span className="font-bold">Description</span>
         <textarea
-          className="px-2 py-1 bg-transparent flex-1 resize-none"
+          className={`${cls$input} flex-1 resize-none`}
           name="description"
         ></textarea>
       </label>
-      <section className="col-start-3 row-start-1 row-span-4 grid place-items-center">
+      <section className="grid place-items-center col-start-3 row-start-1 row-span-4">
         <code className="bg-red-500 aspect-square h-full">
           product-image-picker
         </code>
