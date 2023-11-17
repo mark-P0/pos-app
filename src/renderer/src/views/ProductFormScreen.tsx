5a3e16e50271419f774a83fb3a4201fd76d88901
Mark P <mj.poso18@gmail.com>
2023-11-18 06:12:06 +0800

    Break out fields into separate components

--- src/renderer/src/views/ProductFormScreen.tsx
+++ src/renderer/src/views/ProductFormScreen.tsx
@@ -6,54 +6,91 @@ import {
   cls$interactiveHoverBg,
 } from "@renderer/utils/classes.js";
 
-function ProductFormFieldset() {
-  const cls$label = C(
-    "grid grid-cols-[auto_1fr] items-center gap-3",
-    "p-1 pl-3",
-    cls$card,
-    cls$interactiveHoverBg,
-    "transition",
+const cls$label = C(
+  "grid grid-cols-[auto_1fr] items-center gap-3",
+  "p-1 pl-3",
+  cls$card,
+  cls$interactiveHoverBg,
+  "transition",
+);
+const cls$label$textarea = C(
+  "flex flex-col gap-3",
+  "[&>*:nth-child(2)]:flex-1",
+  "p-2 px-3",
+  cls$card,
+  cls$interactiveHoverBg,
+  "transition",
+);
+const cls$input = C("px-2 py-1", "bg-transparent");
+
+function SKUInput() {
+  return (
+    <label className={`${cls$label} col-span-2`}>
+      <span className="font-bold">SKU</span>
+      <input type="text" className={cls$input} name="sku" />
+    </label>
   );
-  const cls$label$textarea = C(
-    "flex flex-col gap-3",
-    "[&>*:nth-child(2)]:flex-1",
-    "p-2 px-3",
-    cls$card,
-    cls$interactiveHoverBg,
-    "transition",
+}
+
+function NameInput() {
+  return (
+    <label className={`${cls$label} col-span-2`}>
+      <span className="font-bold">Name</span>
+      <input type="text" className={cls$input} name="name" />
+    </label>
   );
-  const cls$input = C("px-2 py-1", "bg-transparent");
+}
+
+function CategoryInput() {
+  return (
+    <label className={`${cls$label} col-span-2`}>
+      <span className="font-bold">Category</span>
+      <input type="text" className={cls$input} name="category" />
+    </label>
+  );
+}
+
+function PriceInput() {
+  return (
+    <label className={cls$label}>
+      <span className="font-bold">Price</span>
+      <input type="number" className={cls$input} name="price" />
+    </label>
+  );
+}
+
+function StockInput() {
+  return (
+    <label className={cls$label}>
+      <span className="font-bold">Stock</span>
+      <input type="number" className={cls$input} name="stock" />
+    </label>
+  );
+}
+
+function DescriptionTextarea() {
+  return (
+    <label
+      className={`${cls$label$textarea} col-span-3 row-[span_18_/_span_18]`}
+    >
+      <span className="font-bold">Description</span>
+      <textarea
+        className={`${cls$input} resize-none`}
+        name="description"
+      ></textarea>
+    </label>
+  );
+}
+
+function Fieldset() {
   return (
     <fieldset className="grid grid-cols-3 gap-3">
-      <label className={`${cls$label} col-span-2`}>
-        <span className="font-bold">SKU</span>
-        <input type="text" className={cls$input} name="sku" />
-      </label>
-      <label className={`${cls$label} col-span-2`}>
-        <span className="font-bold">Name</span>
-        <input type="text" className={cls$input} name="name" />
-      </label>
-      <label className={`${cls$label} col-span-2`}>
-        <span className="font-bold">Category</span>
-        <input type="text" className={cls$input} name="category" />
-      </label>
-      <label className={cls$label}>
-        <span className="font-bold">Price</span>
-        <input type="number" className={cls$input} name="price" />
-      </label>
-      <label className={cls$label}>
-        <span className="font-bold">Stock</span>
-        <input type="number" className={cls$input} name="stock" />
-      </label>
-      <label
-        className={`${cls$label$textarea} col-span-3 row-[span_18_/_span_18]`}
-      >
-        <span className="font-bold">Description</span>
-        <textarea
-          className={`${cls$input} resize-none`}
-          name="description"
-        ></textarea>
-      </label>
+      <SKUInput />
+      <NameInput />
+      <CategoryInput />
+      <PriceInput />
+      <StockInput />
+      <DescriptionTextarea />
       <section className="grid place-items-center col-start-3 row-start-1 row-span-4">
         <code className="bg-red-500 aspect-square h-full">
           product-image-picker
@@ -63,11 +100,11 @@ function ProductFormFieldset() {
   );
 }
 
-function ProductForm() {
+function Form() {
   const cls$button$save = C("px-4 py-1", cls$button$primary, "transition");
   return (
     <form className="h-full flex flex-col [&>*:nth-child(1)]:flex-1 gap-6 p-6 pt-0">
-      <ProductFormFieldset />
+      <Fieldset />
       <section className="grid place-items-end">
         <button type="button" className={cls$button$save}>
           Save
@@ -80,7 +117,7 @@ function ProductForm() {
 export function ProductFormScreen() {
   return (
     <Screen withLogoutButton withFeaturesButton withInventoryManagementButton>
-      <ProductForm />
+      <Form />
     </Screen>
   );
 }
