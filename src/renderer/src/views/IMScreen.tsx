8d7847f405d054dda14bc1d47abbcef38b54312d
Mark P <mj.poso18@gmail.com>
2023-11-17 15:31:41 +0800

    Initialize checkbox buttons abstraction

--- src/renderer/src/views/IMScreen.tsx
+++ src/renderer/src/views/IMScreen.tsx
@@ -1,5 +1,6 @@
 import { ProductList } from "@renderer/components/ProductList.js";
 import { Screen } from "@renderer/components/Screen.js";
+import { useProductsContext } from "@renderer/contexts/ProductsContext.js";
 import {
   C,
   cls$button,
@@ -45,7 +46,57 @@ function RadioButtonFieldset<T extends string>(props: {
   );
 }
 
+function CheckboxButtonFieldset<T extends string>(props: {
+  className: ComponentProps<"fieldset">["className"];
+  values: T[];
+}) {
+  const { values } = props;
+  const [selection, setSelection] = useState<Set<T>>(new Set());
+  function toggle(value: T) {
+    if (selection.has(value)) {
+      selection.delete(value);
+    } else {
+      selection.add(value);
+    }
+    setSelection(new Set(selection));
+  }
+
+  const { className } = props;
+  const attrs$fieldset: ComponentProps<"fieldset"> = { className };
+
+  const cls$button$secondary$selected = C(
+    cls$button,
+    "bg-cyan-600",
+    ...["enabled:hover:bg-cyan-600", "enabled:focus-visible:bg-cyan-600"],
+    "text-white",
+  );
+  const cls$button$checkbox = (value: T) =>
+    C(
+      "px-6 py-3",
+      selection.has(value)
+        ? cls$button$secondary$selected
+        : cls$button$secondary,
+      "transition",
+    );
+  return (
+    <fieldset {...attrs$fieldset}>
+      {values.map((value) => (
+        <button
+          key={value}
+          type="button"
+          className={cls$button$checkbox(value)}
+          onClick={() => toggle(value)}
+        >
+          {value}
+        </button>
+      ))}
+    </fieldset>
+  );
+}
+
 function SortOptions() {
+  const { categories } = useProductsContext();
+
   return (
     <form className="grid gap-6 text-sm">
       <section className="grid gap-3">
@@ -57,6 +108,13 @@ function SortOptions() {
         <RadioButtonFieldset
           className="flex flex-wrap gap-3"
           values={["SKU", "Name", "Price", "Category"]}
+        />
+      </section>
+      <section className="grid gap-3">
+        <header className="font-head text-2xl">Categories:</header>
+        <CheckboxButtonFieldset
+          className="flex flex-wrap gap-3"
+          values={categories}
         />
       </section>
     </form>
