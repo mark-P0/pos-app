32106fbdabf29753e3f8037cdbf5aacbdf393534
Mark P <mj.poso18@gmail.com>
2023-11-21 14:55:24 +0800

    Use Tailwind modifiers for dark mode classes

--- src/renderer/src/components/DarkModeToggle.tsx
+++ src/renderer/src/components/DarkModeToggle.tsx
@@ -7,22 +7,13 @@ export function DarkModeToggle() {
 
   const cls = (() => {
     const button = C("relative", cls$button$icon);
-    const icon = C(
-      "absolute top-0 left-0 p-3",
-      "h-full w-full",
-      "opacity-0",
-      "transition",
-    );
-    const dark = C(icon, "text-cyan-400", status === "dark" && "opacity-100");
-    const light = C(
-      icon,
-      "text-amber-400",
-      status === "light" && "opacity-100",
-    );
+    const icon = C("absolute top-0 left-0 p-3", "h-full w-full", "transition");
+    const dark = C(icon, "text-cyan-400", "opacity-0 dark:opacity-100");
+    const light = C(icon, "text-amber-400", "opacity-100 dark:opacity-0");
     const system = C(
       icon,
       "text-green-400", // In "between" light and dark colors
-      status === "system" && "opacity-100",
+      status === "system" ? "opacity-100" : "opacity-0",
     );
 
     return { button, icon: { dark, light, system } };
