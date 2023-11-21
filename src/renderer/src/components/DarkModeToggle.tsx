fe391f3e3a778ecf352d214eaeaea5ec64110914
Mark P <mj.poso18@gmail.com>
2023-11-21 15:03:13 +0800

    Rename classes to dollar namespacing

--- src/renderer/src/components/DarkModeToggle.tsx
+++ src/renderer/src/components/DarkModeToggle.tsx
@@ -5,24 +5,32 @@ import { LuMoon, LuSun, LuSunMoon } from "react-icons/lu";
 export function DarkModeToggle() {
   const { status, toggle } = useDarkModeContext();
 
-  const cls = (() => {
-    const button = C("relative", cls$button$icon);
-    const icon = C("absolute top-0 left-0 p-3", "h-full w-full", "transition");
-    const dark = C(icon, "text-cyan-400", "opacity-0 dark:opacity-100");
-    const light = C(icon, "text-amber-400", "opacity-100 dark:opacity-0");
-    const system = C(
-      icon,
-      "text-green-400", // In "between" light and dark colors
-      status === "system" ? "opacity-100" : "opacity-0",
-    );
-
-    return { button, icon: { dark, light, system } };
-  })();
+  const cls$button = C("relative", cls$button$icon);
+  const cls$icon = C(
+    "absolute top-0 left-0 p-3",
+    "h-full w-full",
+    "transition",
+  );
+  const cls$icon$dark = C(
+    cls$icon,
+    "text-cyan-400",
+    "opacity-0 dark:opacity-100",
+  );
+  const cls$icon$light = C(
+    cls$icon,
+    "text-amber-400",
+    "opacity-100 dark:opacity-0",
+  );
+  const cls$icon$system = C(
+    cls$icon,
+    "text-green-400", // In "between" light and dark colors
+    status === "system" ? "opacity-100" : "opacity-0",
+  );
   return (
-    <button className={cls.button} onClick={toggle}>
-      <LuMoon className={cls.icon.dark} />
-      <LuSun className={cls.icon.light} />
-      <LuSunMoon className={cls.icon.system} />
+    <button className={cls$button} onClick={toggle}>
+      <LuMoon className={cls$icon$dark} />
+      <LuSun className={cls$icon$light} />
+      <LuSunMoon className={cls$icon$system} />
     </button>
   );
 }
