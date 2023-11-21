d3a6b068179ca237c3a262cbc65086e23d1a5f35
Mark P <mj.poso18@gmail.com>
2023-11-21 16:44:31 +0800

    Merge `chore/minify-builds`

--- electron.vite.config.ts
+++ electron.vite.config.ts
@@ -2,12 +2,22 @@ import react from "@vitejs/plugin-react";
 import { defineConfig, externalizeDepsPlugin } from "electron-vite";
 import { resolve } from "path";
 
+/**
+ * Build not minified by default...
+ * - https://electron-vite.org/config/#built-in-config
+ */
 export default defineConfig({
   main: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      minify: true,
+    },
   },
   preload: {
     plugins: [externalizeDepsPlugin()],
+    build: {
+      minify: true,
+    },
   },
   renderer: {
     resolve: {
@@ -16,5 +26,8 @@ export default defineConfig({
       },
     },
     plugins: [react()],
+    build: {
+      minify: true,
+    },
   },
 });
