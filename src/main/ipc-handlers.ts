8d4fcf7bcc96e4ed43583d863bf548adf80971d8
Mark P <mj.poso18@gmail.com>
2023-11-21 18:29:57 +0800

    Abstract arguments passthrough as spread

--- src/main/ipc-handlers.ts
+++ src/main/ipc-handlers.ts
@@ -44,15 +44,15 @@ const ChannelHandlers = {
     ...{
       "db:isUsernameExisting": async (
         _: IpcMainInvokeEvent,
-        username: Parameters<typeof isUsernameExisting>[0],
+        ...args: Parameters<typeof isUsernameExisting>
       ) => {
-        return await isUsernameExisting(username);
+        return await isUsernameExisting(...args);
       },
       "db:isPasswordCorrect": async (
         _: IpcMainInvokeEvent,
-        password: Parameters<typeof isPasswordCorrect>[0],
+        ...args: Parameters<typeof isPasswordCorrect>
       ) => {
-        return await isPasswordCorrect(password);
+        return await isPasswordCorrect(...args);
       },
     },
     ...{
@@ -61,56 +61,54 @@ const ChannelHandlers = {
       },
       "db:addProduct": async (
         _: IpcMainInvokeEvent,
-        product: Parameters<typeof addProduct>[0],
+        ...args: Parameters<typeof addProduct>
       ) => {
-        return await addProduct(product);
+        return await addProduct(...args);
       },
       "db:deleteProduct": async (
         _: IpcMainInvokeEvent,
-        sku: Parameters<typeof deleteProduct>[0],
+        ...args: Parameters<typeof deleteProduct>
       ) => {
-        await deleteProduct(sku);
+        await deleteProduct(...args);
       },
       "db:editProduct": async (
         _: IpcMainInvokeEvent,
-        product: Parameters<typeof editProduct>[0],
+        ...args: Parameters<typeof editProduct>
       ) => {
-        await editProduct(product);
+        await editProduct(...args);
       },
       "db:isSKUExisting": async (
         _: IpcMainInvokeEvent,
-        sku: Parameters<typeof isSKUExisting>[0],
+        ...args: Parameters<typeof isSKUExisting>
       ) => {
-        return await isSKUExisting(sku);
+        return await isSKUExisting(...args);
       },
     },
   },
   ...{
     "fs:writePngUriToFile": async (
       _: IpcMainInvokeEvent,
-      uri: Parameters<typeof writePngUriToFile>[0],
-      filename: Parameters<typeof writePngUriToFile>[1],
+      ...args: Parameters<typeof writePngUriToFile>
     ) => {
-      return await writePngUriToFile(uri, filename);
+      return await writePngUriToFile(...args);
     },
     "fs:copyFileToTemp": async (
       _: IpcMainInvokeEvent,
-      src: Parameters<typeof copyFileToTemp>[0],
+      ...args: Parameters<typeof copyFileToTemp>
     ) => {
-      return await copyFileToTemp(src);
+      return await copyFileToTemp(...args);
     },
     "fs:copyImageFileToTemp": async (
       _: IpcMainInvokeEvent,
-      filename: Parameters<typeof copyImageFileToTemp>[0],
+      ...args: Parameters<typeof copyImageFileToTemp>
     ) => {
-      return await copyImageFileToTemp(filename);
+      return await copyImageFileToTemp(...args);
     },
     "fs:moveTempFileToImages": async (
       _: IpcMainInvokeEvent,
-      src: Parameters<typeof moveTempFileToImages>[0],
-      dest: Parameters<typeof moveTempFileToImages>[1],
+      ...args: Parameters<typeof moveTempFileToImages>
     ) => {
-      return await moveTempFileToImages(src, dest);
+      return await moveTempFileToImages(...args);
     },
   },
 } as const;
