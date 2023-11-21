e82dee78c43edbe61482c711a86cd5a02e015323
Mark P <mj.poso18@gmail.com>
2023-11-21 16:07:30 +0800

    Make dir initialization blocking

--- src/main/init.ts
+++ src/main/init.ts
@@ -1,21 +1,21 @@
-import { access, mkdir } from "fs/promises";
+import { accessSync, mkdirSync } from "fs";
 import { getActualFilePath } from "../../data/utils.js";
 
-async function isDirExisting(dir: string) {
+function isPathExisting(dir: string) {
   try {
-    await access(dir);
+    accessSync(dir);
     return true;
   } catch {
     return false;
   }
 }
 
-async function initializeDirectories() {
+function initializeDirectories() {
   const dirs = ["data", "data/images", "data/receipts", "data/temp"];
   for (const dir of dirs) {
     const absolute = getActualFilePath(dir);
-    if (await isDirExisting(absolute)) continue;
-    await mkdir(absolute);
+    if (isPathExisting(absolute)) continue;
+    mkdirSync(absolute);
   }
 }
 
