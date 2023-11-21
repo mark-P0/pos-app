9cafdcd87528f079de8df79015c583d5de1b99f5
Mark P <mj.poso18@gmail.com>
2023-11-21 16:24:45 +0800

    Create DB file if non-existent

--- data/schema.ts
+++ data/schema.ts
@@ -1,3 +1,6 @@
+import Database from "better-sqlite3";
+import { sql } from "drizzle-orm";
+import { drizzle } from "drizzle-orm/better-sqlite3";
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
 export const products = sqliteTable("products", {
@@ -18,3 +21,33 @@ export const users = sqliteTable("users", {
 });
 export type User = typeof users.$inferSelect;
 export type NewUser = typeof users.$inferInsert;
+
+/**
+ * Reflect the schemas above in the statements below!
+ *
+ * Can take a look inside a pre-existing SQLite file
+ */
+export function createDb(path: string) {
+  const sqlite = new Database(path);
+  const db = drizzle(sqlite);
+
+  db.run(sql`
+    CREATE TABLE users (
+      username TEXT NOT NULL,
+      password TEXT NOT NULL,
+      PRIMARY KEY (username)
+    )
+  `);
+  db.run(sql`
+    CREATE TABLE products (
+      sku TEXT NOT NULL,
+      category TEXT NOT NULL,
+      name TEXT NOT NULL,
+      description TEXT NOT NULL,
+      price INTEGER NOT NULL,
+      stock INTEGER NOT NULL,
+      url TEXT,
+      PRIMARY KEY (sku)
+    )
+  `);
+}
